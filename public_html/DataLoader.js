df3262116a95c2857e0b4f5b30384713cc52b59e
cleonty <leonty@inbox.ru>
2016-06-17 11:23:50 +0600

    First gradient boosting working version

--- public_html/DataLoader.js
+++ public_html/DataLoader.js
@@ -105,15 +105,118 @@
     return canvasAngularElement;
   };
 
+  NumberImage.prototype.toArray = function () {
+    var len = this.image.byteLength, i, arr = new Array(len);
+    for (i = 0; i < len; i++) {
+      arr[i] = this.image[i];
+    }
+    return arr;
+  };
+
   function NeuralNetwork(inputLayerSize, hiddenLayerSize, outputLayerSize) {
     this.inputLayer = new Layer(inputLayerSize);
     this.hiddenLayer = new Layer(hiddenLayerSize);
     this.outputLayer = new Layer(outputLayerSize);
-    
+
     this.inputLayer.connectTo(this.hiddenLayer);
     this.hiddenLayer.connectTo(this.outputLayer);
+    this.trace = false;
     console.log(this);
   }
+
+  NeuralNetwork.prototype.feedForward = function (a) {
+    var results = [];
+    a = this.inputLayer.activate(a);
+    if (this.trace) {
+      results.push(a);
+    }
+    a = this.hiddenLayer.activate(a);
+    if (this.trace) {
+      results.push(a);
+    }
+    a = this.outputLayer.activate(a);
+    if (this.trace) {
+      results.push(a);
+      console.table(results);
+    }
+    return a;
+  };
+
+  NeuralNetwork.prototype.SGD = function (trainingData, epochs, miniBatchSize, eta, testDataOrUndefined) {
+    var i, j, nTest, n;
+    if (testDataOrUndefined) {
+      nTest = testDataOrUndefined.length;
+    }
+    n = trainingData.length;
+    for (i = 0; i < epochs; i++) {
+      console.groupCollapsed('Epoch %d', i);
+      trainingData.shuffle();
+      var miniBatches = [];
+      for (j = 0; j < n; j += miniBatchSize) {
+        miniBatches.push(trainingData.slice(j, j + miniBatchSize));
+      }
+      for (j = 0; j < miniBatches.length; j++) {
+        this.updateMiniBatch(miniBatches[j], eta);
+      }
+      if (testDataOrUndefined) {
+        console.log("Epoch %d: %d / %d", i, this.evaluate(testDataOrUndefined), nTest);
+      } else {
+        console.log("Epoch %d complete", i);
+      }
+      console.groupEnd('Epoch %d', i);
+    }
+  };
+
+  NeuralNetwork.prototype.updateMiniBatch = function (miniBatch, eta) {
+    console.groupCollapsed('minibatch');
+    this.hiddenLayer.initNablaBiases();
+    this.hiddenLayer.initNablaWeights();
+    this.outputLayer.initNablaBiases();
+    this.outputLayer.initNablaWeights();
+    var miniBatchSize = miniBatch.length;
+    for (var i = 0; i < miniBatchSize; i++) {
+      this.backprop(miniBatch[i][0], miniBatch[i][1]);
+    }
+    this.hiddenLayer.updateBiases(miniBatchSize, eta);
+    this.hiddenLayer.updateWeights(miniBatchSize, eta);
+    this.outputLayer.updateBiases(miniBatchSize, eta);
+    this.outputLayer.updateWeights(miniBatchSize, eta);
+    console.groupEnd('minibatch');
+  };
+
+  NeuralNetwork.prototype.backprop = function (x, y) {
+//    this.hiddenLayer.initNablaBiases();
+//    this.hiddenLayer.initNablaWeights();
+//    this.outputLayer.initNablaBiases();
+//    this.outputLayer.initNablaWeights();
+
+    this.feedForward(x);
+
+    var delta = this.outputLayer.mul(this.outputLayer.costDerivative(this.outputLayer.output, y), this.outputLayer.sigmoidPrime(this.outputLayer.z));
+    for (var i = 0; i < delta.length; i++) {
+      this.outputLayer.nablaWeights[i] = this.outputLayer.sum(this.outputLayer.nablaWeights[i], this.outputLayer.mulToScalar(this.hiddenLayer.output, delta[i]));
+    }
+
+    var sp = this.hiddenLayer.sigmoidPrime(this.hiddenLayer.z);
+    //var delta = this.hiddenLayer.sum(this.hiddenLayer.dot(this.outputLayer.weights, delta), sp);
+    var delta2 = [];
+    for (var i = 0; i < this.outputLayer.weights.length; i++) {
+      var a = this.hiddenLayer.sum(this.hiddenLayer.mulToScalar(this.outputLayer.weights[i], delta[i]), sp);
+      if (i == 0) {
+        delta2 = a;
+      } else {
+        delta2 = this.hiddenLayer.sum(delta2, a);
+      }
+    }
+    this.hiddenLayer.nablaBiases = this.hiddenLayer.sum(this.hiddenLayer.nablaBiases, delta2);
+    for (var i = 0; i < this.hiddenLayer.nablaWeights.length; i++) {
+      this.hiddenLayer.nablaWeights[i] = this.hiddenLayer.sum(this.hiddenLayer.nablaWeights[i], this.hiddenLayer.mulToScalar(this.inputLayer.activation, delta2[i]));
+    }
+  };
+
+  NeuralNetwork.prototype.evaluate = function (testData) {
+    return 1;
+  };
 
   function Layer(size) {
     this.size = size;
@@ -121,44 +224,284 @@
     this.weights;
     this.nextLayer;
     this.previousLayer;
+    this.nablaBiases;
+    this.nablaWeights;
+    this.activation;
+    this.z;
+    this.output;
   }
 
   Layer.prototype.initBiases = function () {
     this.biases = new Array(this.size);
     for (var i = 0; i < this.size; i++) {
-      this.biases[i] = Math.random() * 0.2 - 0.1;
+      this.biases[i] = this.random();
     }
   };
-  
-  Layer.prototype.connectTo = function(nextLayer) {
+
+  Layer.prototype.initNablaBiases = function () {
+    var nablaBiases;
+    nablaBiases = new Array(this.size);
+    for (var i = 0; i < this.size; i++) {
+      nablaBiases[i] = 0;
+    }
+    this.nablaBiases = nablaBiases;
+  };
+
+  Layer.prototype.connectTo = function (nextLayer) {
     this.nextLayer = nextLayer;
     nextLayer.previousLayer = this;
     nextLayer.initBiases();
     nextLayer.initWeights();
   };
-  
+
   Layer.prototype.initWeights = function () {
     var previousLayerSize = this.previousLayer.size;
     var weights = new Array(this.size);
     for (var i = 0; i < this.size; i++) {
       weights[i] = new Array(previousLayerSize);
       for (var j = 0; j < previousLayerSize; j++) {
-        weights[i][j] = Math.random() * 0.2 - 0.1;
+        weights[i][j] = this.random();
       }
     }
     this.weights = weights;
   };
 
+  Layer.prototype.initNablaWeights = function () {
+    var nablaWeights;
+    if (this.weights) {
+      var previousLayerSize = this.previousLayer.size;
+      nablaWeights = new Array(this.size);
+      for (var i = 0; i < this.size; i++) {
+        nablaWeights[i] = new Array(previousLayerSize);
+        for (var j = 0; j < previousLayerSize; j++) {
+          nablaWeights[i][j] = 0;
+        }
+      }
+    }
+    this.nablaWeights = nablaWeights;
+  };
+
+  Layer.prototype.updateWeights = function (miniBatchSize, eta) {
+    var weights = this.weights, len = weights.length, nablaWeights = this.nablaWeights, factor = eta / miniBatchSize;
+
+    for (var i = 0; i < len; i++) {
+      //weights[i] = (weights[i] - eta / miniBatchSize) * nablaWeights[i];
+      weights[i] = this.sub(weights[i], this.mulToScalar(nablaWeights[i], factor))
+      //weights[i] = (weights[i] - eta / miniBatchSize) * nablaWeights[i];
+    }
+    this.weights = weights;
+  };
+
+  Layer.prototype.updateBiases = function (miniBatchSize, eta) {
+    var biases = this.biases, len = biases.length, nablaBiases = this.nablaBiases;
+    for (var i = 0; i < len; i++) {
+      biases[i] = (biases[i] - eta / miniBatchSize) * nablaBiases[i];
+    }
+    this.biases = biases;
+  };
+
+  Layer.prototype.random = function () {
+    return Math.random() * 0.2 - 0.1;
+  };
+
+  Layer.prototype.activate = function (a) {
+    var result;
+    this.activation = a;
+    if (this.weights && this.biases) {
+      result = this.z = this.wab(a);
+      result = this.output = this.sigmoid(result);
+    } else {
+      result = this.output = a;
+    }
+    return result;
+  };
+
+  Layer.prototype.wab = function (a) {
+    var size = this.size;
+    var result = new Array(size);
+    for (var i = 0; i < size; i++) {
+      result[i] = this.dot(this.weights[i], a);
+    }
+    return this.sum(result, this.biases);
+  };
+
+  Layer.prototype.dot = function (a, b) {
+    var length = a.length,
+            i = 0,
+            result = 0;
+    for (i = 0; i < length; i++) {
+      result += a[i] * b[i];
+    }
+    return result;
+  };
+
+  Layer.prototype.dot2 = function (a, b) {
+    var length = a.length,
+            i = 0,
+            result = 0;
+    for (i = 0; i < length; i++) {
+      result += a[i] * b[i];
+    }
+    return result;
+  };
+
+  Layer.prototype.sum = function (a, b) {
+    var length = a.length,
+            i = 0,
+            result = new Array(length);
+    for (i = 0; i < length; i++) {
+      result[i] = a[i] + b[i];
+    }
+    return result;
+  };
+
+  Layer.prototype.mul = function (a, b) {
+    var length = a.length,
+            i = 0,
+            result = new Array(length);
+    for (i = 0; i < length; i++) {
+      result[i] = a[i] * b[i];
+    }
+    return result;
+  };
+
+  Layer.prototype.mulToScalar = function (a, b) {
+    var length = a.length,
+            i = 0,
+            result = new Array(length);
+    for (i = 0; i < length; i++) {
+      result[i] = a[i] * b;
+    }
+    return result;
+  };
+
+  Layer.prototype.sub = function (a, b) {
+    var length = a.length,
+            i = 0,
+            result = new Array(length);
+    for (i = 0; i < length; i++) {
+      result[i] = a[i] - b[i];
+    }
+    return result;
+  };
+
+  Layer.prototype.costDerivative = function (a, b) {
+    return this.sub(a, b);
+  };
+
+  Layer.prototype.sigmoid = function (z) {
+    var result;
+    if (Array.isArray(z)) {
+      var len = z.length, i = 0,
+              result = new Array(len);
+      for (i = 0; i < len; i++) {
+        result[i] = this.sigmoid(z[i]);
+      }
+    } else {
+      result = 1.0 / (1.0 + Math.exp(-z));
+    }
+    return result;
+  };
+
+  Layer.prototype.sigmoidPrime = function (z) {
+    var result;
+    if (Array.isArray(z)) {
+      var len = z.length, i = 0,
+              result = new Array(len);
+      for (i = 0; i < len; i++) {
+        result[i] = this.sigmoidPrime(z[i]);
+      }
+    } else {
+      result = this.sigmoid(z) * (1.0 - this.sigmoid(z));
+    }
+    return result;
+  };
+
   var dataLoader = new DataLoader();
   dataLoader.loadLabelsAndImages(function () {
     var imageCount = dataLoader.getImageCount();
-    for (var i = 0; i < 50; i++) {
+    var traningData = [];
+    var n = 600;
+    for (var i = 0; i < n; i++) {
       var image = dataLoader.getImage(i);
       var canvasAngularElement = image.createElement();
       angular.element(document.body).append(canvasAngularElement);
+      var input = image.toArray();
+      var output = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      output[image.label] = 1;
+      traningData.push([input, output]);
     }
+    //console.table(traningData);
+    var net = new NeuralNetwork(input.length, 30, 10);
+    net.SGD(traningData, 50, 10, 3.0);
+    var passed = 0, failed = 0, total = 0;
+    for (var i = 0; i < n; i++) {
+      var image = dataLoader.getImage(i);
+      var input = image.toArray();
+      var output = net.feedForward(input);
+      var expected = image.label, actual = output.indexOfMaxElement(), success = expected === actual;
+      if (success) {
+        passed++;
+      } else {
+        failed++;
+      }
+      total++;
+      console.log("expected %d, actual %d, success %s", expected, actual, success);
+    }
+    console.log("passed %d, failed %d, total %d", passed, failed, total);
   });
-  var net = new NeuralNetwork(3, 3, 3);
+
+  Array.prototype.indexOfMaxElement = function () {
+    var len = this.length, maxElementIndex = 0, maxElement = this[maxElementIndex], i;
+    for (var i = 1; i < len; i++) {
+      if (this[i] > maxElement) {
+        maxElement = this[i];
+        maxElementIndex = i;
+      }
+    }
+    return maxElementIndex;
+  }
+
+  Array.prototype.shuffle = function () {
+    var len = this.length, randomIndex, i = 0;
+    for (var i = 0; i < len; i++) {
+      randomIndex = Math.floor(Math.random() * len);
+      if (i !== randomIndex) {
+        var tmp = this[randomIndex];
+        this[randomIndex] = this[i];
+        this[i] = tmp;
+      }
+    }
+  };
+
+  Array.prototype.transpose = function () {
+    var rowCount = this.length;
+    var colCount = this[0].length;
+    var transposed = new Array(colCount);
+    for (var i = 0; i < colCount; i++) {
+      transposed[i] = new Array(rowCount);
+      for (var j = 0; j < rowCount; j++) {
+        transposed[i][j] = this[j][i];
+      }
+    }
+    return transposed;
+  };
+
+  function testShuffle() {
+    var arr = [1, 2, 3, 4, 5, 6, 7];
+    arr.shuffle();
+    console.table([arr]);
+  }
+
+  function testTranspose() {
+    var arr = [
+      [1, 2],
+      [4, 5],
+      [7, 8]
+    ];
+    var transposed = arr.transpose();
+    console.table(transposed);
+  }
 })(console, angular.module('neuralNetworkApp', []), angular, document, Math, 1);
 
 
